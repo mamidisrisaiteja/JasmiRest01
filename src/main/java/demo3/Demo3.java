7b0b6cd1eefbc362cf9d3e6d7a33155d029e2ebd
Sri Sai Teja Mamidi <Sri.Mamidi@in.ey.com>
2022-07-02 15:05:32 +0530

    test

---
+++ src/main/java/demo3/Demo3.java
@@ -0,0 +1,39 @@
+package demo3;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		RestAssured.baseURI="https://rahulshettyacademy.com";//we will specify base URI
+		
+		RequestSpecification res=RestAssured.given();//declared the request object
+		
+		Response response=  res.queryParam("key","qaclick123")
+				.headers("Content-Type","application/json")
+				.body("{\r\n"
+						+ "  \"location\": {\r\n"
+						+ "    \"lat\": -38.383494,\r\n"
+						+ "    \"lng\": 33.427362\r\n"
+						+ "  },\r\n"
+						+ "  \"accuracy\": 100,\r\n"
+						+ "  \"name\": \"Frontline house\",\r\n"
+						+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+						+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+						+ "  \"types\": [\r\n"
+						+ "    \"shoe park\",\r\n"
+						+ "    \"shop\"\r\n"
+						+ "  ],\r\n"
+						+ "  \"website\": \"http://google.com\",\r\n"
+						+ "  \"language\": \"French-IN\"\r\n"
+						+ "}")
+				.post("maps/api/place/add/json");
+		response.prettyPrint();
+				
+				
+
+	}
+
+}
